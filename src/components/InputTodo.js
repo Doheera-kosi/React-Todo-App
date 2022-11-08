f3e63c930607ff2441feb57c02a5de928c42d3e6
Doheera-kosi <kupourevans99@gmail.com>
2022-11-08 10:26:41 +0000

    Created React Todo App

---
+++ src/components/InputTodo.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class InputTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { title } = this.state;
+    const { addTodoProps } = this.props;
+    if (title.trim()) {
+      addTodoProps(title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  }
+
+  render() {
+    const { title } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add Todo..."
+          value={title}
+          onChange={this.onChange}
+        />
+        <button type="button" className="input-submit">Submit</button>
+      </form>
+    );
+  }
+}
+InputTodo.propTypes = {
+  addTodoProps: PropTypes.func.isRequired,
+};
+export default InputTodo;
